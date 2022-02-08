b80d393db668d1bf82d58d4aa62e784e77b2c8e2
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-08 23:57:58 +0900

    update--ogp image url

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
       <Seo
         pageTitle="TOP"
         pageDescription="TailwindCSSのPrefixを各クラス名の先頭に付与します。"
-        pageImg="/ogp.png"
+        pageImg="https://twc-prefix-parser.vercel.app/ogp.png"
         pageImgWidth={1280}
         pageImgHeight={640}
       />
